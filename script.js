09d0507994e9e51e184778b7bc1bd00c2f77f2b6
mannyAddisu <codesbycodex@gmail.com>
2024-02-24 23:52:23 +0300

    settings menu click event update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const settingsMenu = document.querySelector(".settings-menu");
+const profile = document.querySelector(".nav-img");
 function settingsToggle() {
   settingsMenu.classList.toggle("settings-menu-height");
 }
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key == "Escape" &&
+    settingsMenu.classList.contains("settings-menu-height")
+  ) {
+    settingsMenu.classList.remove("settings-menu-height");
+  }
+});
+
+window.addEventListener("click", function (e) {
+  if (!settingsMenu.contains(e.target) && !profile.contains(e.target)) {
+    settingsMenu.classList.remove("settings-menu-height");
+  }
+});
