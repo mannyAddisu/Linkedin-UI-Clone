cc6a2c783dfe3d9bf7b12b5ac50a66c7d80f9924
mannyAddisu <codesbycodex@gmail.com>
2024-02-25 23:36:45 +0300

    sort row update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const settingsMenu = document.querySelector(".settings-menu");
 const profile = document.querySelector(".nav-img");
+const select = document.querySelector(".select");
+const option = document.querySelector(".options");
 function settingsToggle() {
   settingsMenu.classList.toggle("settings-menu-height");
 }
@@ -16,5 +18,23 @@ document.addEventListener("keydown", function (e) {
 window.addEventListener("click", function (e) {
   if (!settingsMenu.contains(e.target) && !profile.contains(e.target)) {
     settingsMenu.classList.remove("settings-menu-height");
+  }
+});
+
+select.addEventListener("click", function () {
+  if (option.style.display === "none") {
+    option.style.display = "block";
+  } else {
+    option.style.display = "none";
+  }
+});
+option.addEventListener("click", function (e) {
+  option.style.display = "none";
+  if (e.target == document.getElementById("op1")) {
+    document.getElementById("select-p").textContent =
+      document.getElementById("op1").textContent;
+  } else {
+    document.getElementById("select-p").textContent =
+      document.getElementById("op2").textContent;
   }
 });
